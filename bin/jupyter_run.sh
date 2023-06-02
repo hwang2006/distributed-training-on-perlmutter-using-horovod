8792fc03606a47c1c2cac2ca5ade04a9a241dab6
Soonwook Hwang <hwang@kisti.re.kr>
2023-06-02 22:47:51 +0900

    Initial commit

--- bin/jupyter_run.sh
+++ bin/jupyter_run.sh
@@ -33,10 +33,12 @@ module load  cudnn/8.7.0  nccl/2.15.5-ofi  evp-patch
 
 echo "execute jupyter"
 source ~/.bashrc
+
 #conda activate horovod
 conda activate craympi-hvd
 #cd $SCRATCH/ddl-projects
 cd $SCRATCH
+
 jupyter lab --ip=0.0.0.0 --port=${PORT_JU} --NotebookApp.token=${USER}
 #bash -c "jupyter lab --ip=0.0.0.0 --port=${PORT_JU} --NotebookApp.token='${USER}'"
 echo "end of the job"
